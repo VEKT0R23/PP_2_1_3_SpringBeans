389c53052f34f70d50a3e231f476d61e69a76730
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:47:25 +0300

    pom update

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -11,10 +11,12 @@ public class Application {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage timer = applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
+
     }
 
 }
